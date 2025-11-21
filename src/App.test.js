e60952f70655ab02debd69d4219f71ef79bb81f7
Aldo Luna <aldoyamil23@hotmail.com>
2025-11-21 16:32:05 -0600

    Add new dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders blue green deployment text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Blue-Green Deployment Demo/i);
   expect(linkElement).toBeInTheDocument();
 });
